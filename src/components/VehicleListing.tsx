386413e8f107c955d3255f08daa37f84eb7f11bd
kumarr-abhi <kumarr.abhi@hotmail.com>
2025-02-21 00:20:16 +0100

    Add VehicleListing.tsx with skeleton divs for vehicle listing

---
+++ src/components/VehicleListing.tsx
@@ -0,0 +1,14 @@
+export default function VehicleListing() {
+  return <div>
+    <div className="flex">
+        <div>Searchbar</div>
+        <div>Sort By</div>
+    </div>
+    <div>
+        Listing
+    </div>
+    <div>
+        Pagination section
+    </div>
+  </div>;
+}
